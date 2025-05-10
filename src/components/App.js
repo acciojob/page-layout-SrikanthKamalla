f29e7dc5bac812e283bff515328d2207a01f3c29
Srikanth Kamalla <srikanthkamalla2919@gmail.com>
2025-05-10 11:54:09 +0530

    push 2

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import PageLayout from "./PageLayout";
 const App = () => {
   return (
     <div>
-      {/* Do not remove the main div */}
+      {/* Do not remove the main div js */}
       <PageLayout
         header={<h1>Welcome to my website</h1>}
         footer={<p>&copy; 2023 My Website. All rights reserved.</p>}
